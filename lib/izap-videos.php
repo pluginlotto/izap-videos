4c8b3da2639ab15d8ed655e313aa1155c41ced80
priya-izap <priya.garg@izap.in>
2014-09-19 16:55:54 -0400

    print id

--- lib/izap-videos.php
+++ lib/izap-videos.php
@@ -294,7 +294,7 @@
 //      )));
       exit;
     }
-    $id = get_input('id');
+    $id = get_input('id');echo $id;exit;
     $pass = '%kdkdhSw*jdksl';
     forward(elgg_add_action_tokens_to_url(elgg_get_site_url() .'action/izap-videos/save?id=' . $id . '&p=' . $pass));
     exit;
