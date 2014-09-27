70f4927b9da6a81dd534469b1358abe38dfa4356
Priya-iZAP <priya.garg@izap.in>
2014-09-27 17:30:28 +0530

    add isset with error

--- lib/izap-videos.php
+++ lib/izap-videos.php
@@ -725,11 +725,11 @@
       foreach ($queue as $pending) {
         $converted = izapConvertVideo_izap_videos($pending['main_file'], $pending['guid'], $pending['title'], $pending['url'], $pending['owner_id']);
         $izap_video = get_entity($pending['guid']);
-        if ($converted['error']) { 
-          $izap_video->converted = 'no';
+        if (isset($converted['error'])) { 
+          $izap_video->converted = 'no00';
           $queue_object->move_to_trash($pending['guid']);
         } else { 
-          $izap_video->converted = 'yes';
+          $izap_video->converted = 'yesSS';
           $queue_object->delete($pending['guid']);
         }
       }
