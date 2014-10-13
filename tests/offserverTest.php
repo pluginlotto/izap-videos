b689d8fd46ba7310929a73927f7e3d792a91c5d3
monika-mni <monika.mni@izap.in>
2014-10-13 17:39:20 +0530

    change video url for offserver unit test

--- tests/offserverTest.php
+++ tests/offserverTest.php
@@ -31,12 +31,12 @@ class offserverTest extends PHPUnit_Framework_TestCase {
 		$tags = "offserver,video";
 		$data = array(
 			'subtype' => GLOBAL_IZAP_VIDEOS_SUBTYPE,
-			'title' => 'Apple Music Special Event 2005-The iPod Nano Introduction',
-			'description' => 'Here we see Steve Jobs introducing the first ever iPod Nano.',
+			'title' => 'Barack Obama and Narendra Modi - Joint Briefing from White House',
+			'description' => 'Prime Minister Narendra Modi on Tuesday (September 30) arrived at the White House to hold talks with US President Barack Obama. The two are expected to issue a joint statement at the end of the meeting. Both leaders will be discussing issues ranging from manufacturing to sanitation to deepen ties.',
 			'access_id' => 2,
 			'container_guid' => 77,
 			'owner_guid' => 77,
-			'videourl' => 'https://www.youtube.com/watch?v=7GRv-kv5XEg',
+			'videourl' => 'https://www.youtube.com/watch?v=uDYarhCmvfM',
 			'videoprocess' => 'offserver',
 			'tags' => string_to_tag_array($tags)
 		);
@@ -47,16 +47,16 @@ class offserverTest extends PHPUnit_Framework_TestCase {
 		 * Expected Result
 		 */
 		$output = new IzapVideo();
-		$output->videourl = array('https://www.youtube.com/watch?v=7GRv-kv5XEg');
+		$output->videourl = array('https://www.youtube.com/watch?v=uDYarhCmvfM');
 		$output->videoprocess = array('offserver');
 		$output->tags = array('offserver', 'video');
-		$output->videothumbnail = array('http://i.ytimg.com/vi/7GRv-kv5XEg/1.jpg');
-		$output->videosrc = array('<iframe width="800" height="500" src="http://www.youtube.com/embed/7GRv-kv5XEg?autoplay=1&amp;wmode=transparent" frameborder="0"></iframe>');
-		$output->domain = array('https://www.youtube.com/watch?v=7GRv-kv5XEg');
+		$output->videothumbnail = array('http://i.ytimg.com/vi/uDYarhCmvfM/1.jpg');
+		$output->videosrc = array('<iframe width="800" height="500" src="http://www.youtube.com/embed/uDYarhCmvfM?autoplay=1&amp;wmode=transparent" frameborder="0"></iframe>');
+		$output->domain = array('https://www.youtube.com/watch?v=uDYarhCmvfM');
 		$output->video_type = array('youtube');
 		$output->orignal_thumb = array('izap_videos/tmp/original_');
 		$output->imagesrc = array('izap_videos/tmp/');
-		$output->videotype_site = array('https://www.youtube.com/watch?v=7GRv-kv5XEg');
+		$output->videotype_site = array('https://www.youtube.com/watch?v=uDYarhCmvfM');
 		$output->converted = array('yes');
 		$output->filename = array('izap_videos/tmp/original_');
 
@@ -66,8 +66,8 @@ class offserverTest extends PHPUnit_Framework_TestCase {
 		$output->container_guid = 77;
 		$output->access_id = 2;
 		$output->enabled = 'yes';
-		$output->title = 'Apple Music Special Event 2005-The iPod Nano Introduction';
-		$output->description = 'Here we see Steve Jobs introducing the first ever iPod Nano.';
+		$output->title = 'Barack Obama and Narendra Modi - Joint Briefing from White House';
+		$output->description = 'Prime Minister Narendra Modi on Tuesday (September 30) arrived at the White House to hold talks with US President Barack Obama. The two are expected to issue a joint statement at the end of the meeting. Both leaders will be discussing issues ranging from manufacturing to sanitation to deepen ties.';
 
 		$this->assertEquals($output, $izap_videos);
 	}
