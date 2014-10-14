2ee41790a93946dd7926c82d87ce8ad87c5f2666
monika-mni <monika.mni@izap.in>
2014-10-14 10:36:16 +0530

    start working on YouTube video unit test

---
+++ tests/youtubeTest.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of youtubeTest
+ *
+ * @author monika
+ */
+class youtubeTest extends PHPUnit_Framework_TestCase{
+	
+	public function testYouTube() {
+		
+	}
+}
