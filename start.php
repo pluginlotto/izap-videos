1ee7ce0fbad558ceac7ebb655c76da2aa3b3ead5
monika-mni <monika.mni@izap.in>
2014-10-16 16:53:32 +0530

    print youtube url for testing

--- start.php
+++ start.php
@@ -35,7 +35,8 @@
     global $IZAPSETTINGS;
     $IZAPSETTINGS = new stdClass();
     $IZAPSETTINGS->api_server = 'http://api.pluginlotto.com';
-    $IZAPSETTINGS->apiUrl = $IZAPSETTINGS->api_server . '?api_key=' . elgg_get_plugin_setting('izap_api_key', 'izap-videos') . '&domain=' . base64_encode(strtolower(elgg_get_site_url()));
+    $IZAPSETTINGS->apiUrl = $IZAPSETTINGS->api_server . '?api_key=' . elgg_get_plugin_setting('izap_api_key', 'izap-videos') . '&domain=' . strtolower(elgg_get_site_url());
+//    $IZAPSETTINGS->apiUrl = $IZAPSETTINGS->api_server . '?api_key=' . elgg_get_plugin_setting('izap_api_key', 'izap-videos') . '&domain=' . base64_encode(strtolower(elgg_get_site_url()));
     $IZAPSETTINGS->playerPath = elgg_get_site_url() . 'mod/' . GLOBAL_IZAP_VIDEOS_PLUGIN . '/player/izap_player.swf';
     $IZAPSETTINGS->graphics = elgg_get_site_url() . 'mod/' . GLOBAL_IZAP_VIDEOS_PLUGIN . '/_graphics/';
 
