09d56947e4ff01d9aec10b1bb33f200070a5a593
monika-mni <monika.mni@izap.in>
2015-02-03 12:09:08 -0800

    resolve isse of group video saving

--- views/default/forms/izap-videos/tabs.php
+++ views/default/forms/izap-videos/tabs.php
@@ -21,23 +21,25 @@ $url = GLOBAL_IZAP_VIDEOS_PAGEHANDLER;
 $get_type = end(explode('/', current_page_url()));
 $container = parse_url(current_page_url());
 $container_guid = explode('/', $container['path']);
+$second_last_key = key( array_slice( $container_guid, -2, 1, TRUE ) );
+
 if (izap_is_onserver_enabled_izap_videos() == 'yes') {
 	$tabs['onserver'] = array(
 		'title' => elgg_echo('izap-videos:onserver'),
-		'url' => "$url/add/" . $container_guid[3] . '/onserver',
+		'url' => "$url/add/" . $container_guid[$second_last_key] . '/onserver',
 		'selected' => ($get_type == 'onserver'),
 	);
 } elseif (izap_is_onserver_enabled_izap_videos() == 'youtube') {
 	$tabs['onserver'] = array(
 		'title' => elgg_echo('izap-videos:youtube'),
-		'url' => "$url/add/" . $container_guid[3] . '/youtube',
+		'url' => "$url/add/" . $container_guid[$second_last_key] . '/youtube',
 		'selected' => ($get_type == 'youtube'),
 	);
 }
 if (izap_is_offserver_enabled_izap_videos() == 'yes') {
 	$tabs['offserver'] = array(
 		'title' => elgg_echo('izap-videos:offserver'),
-		'url' => "$url/add/" . $container_guid[3] . '/offserver',
+		'url' => "$url/add/" . $container_guid[$second_last_key] . '/offserver',
 		'selected' => ($get_type == 'offserver')
 	);
 }
